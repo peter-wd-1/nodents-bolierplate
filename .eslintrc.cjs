203a9e3cd9de362abeba605afe343b933ea00f53
peterismic@gmail.com <peterismic@gmail.com>
2022-03-16 15:31:09 -0400

    let's get started

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+    'jest/globals': true,
+  },
+  plugins: ['prettier', 'jest', 'markdown'],
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:jest/all',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'jest/expect-expect': 'off',
+    'jest/prefer-expect-assertions': 'off',
+    'jest/no-test-return-statement': 'off',
+    'import/prefer-default-export': 'off',
+    'semi':'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+}
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { add } from 'ad/adder.ts'
+import express from 'express'
+
+exports = {}
+const app = express()
+
+app.listen(3000, () => {
+  console.log(`Server Listening on port ${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('hello')
+  process.exit(1)
+})
+
+const arr = [
+  [0, 1],
+  [2, 3],
+  [4, 5],
+]
+
+var a = 1
+console.log(a)
+console.log({ route })
+console.log(`add('1 2') = ${add('1 2')}`)
